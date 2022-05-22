a669f8aab012a761b0e540b58608c1e08cf1aaf2
asif a owadud <asifaowadud@gmail.com>
2022-05-22 06:30:57 +0600

    first commit

---
+++ src/firebase.init.js
@@ -0,0 +1,46 @@
+// // Import the functions you need from the SDKs you need
+// import { initializeApp } from "firebase/app";
+// import { getAuth } from "firebase/auth";
+// // TODO: Add SDKs for Firebase products that you want to use
+// // https://firebase.google.com/docs/web/setup#available-libraries
+
+// // Your web app's Firebase configuration
+// const firebaseConfig = {
+//   apiKey: "AIzaSyCbT-rDKMEMf1ou8XbyO1-DypjKRP4nBbk",
+//   authDomain: "loyalcars12.firebaseapp.com",
+//   projectId: "loyalcars12",
+//   storageBucket: "loyalcars12.appspot.com",
+//   messagingSenderId: "821523965996",
+//   appId: "1:821523965996:web:86581611d2f8b820cc8989",
+// };
+
+// // Initialize Firebase
+// const app = initializeApp(firebaseConfig);
+// const auth = getAuth(app)
+
+// export default auth;
+
+
+
+
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: process.env.REACT_APP_apiKey,
+  authDomain: process.env.REACT_APP_authDomain,
+  projectId: process.env.REACT_APP_projectId,
+  storageBucket: process.env.REACT_APP_storageBucket,
+  messagingSenderId: process.env.REACT_APP_messagingSenderId,
+  appId: process.env.REACT_APP_appId,
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app)
+
+export default auth;
